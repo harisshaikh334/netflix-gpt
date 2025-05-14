b31a258143381adca04097fe999c2c3b634a989a
Mohammed Harish <harish_m@xoriant.com>
2025-05-14 02:45:26 -0700

    setup basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Body from "./components/Body";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-      Hello every one lets build our first project.
-    </h1>
+    <Body />
   )
 }
 
